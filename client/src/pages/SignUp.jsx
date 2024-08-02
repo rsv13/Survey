db57a6ccb755f167f25fadcd7f0bbe68615d0c38
rsv13 <rsv130393@gmail.com>
2024-08-03 00:12:57 +0100

    Add functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,47 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim() })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.username  || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        return setErrorMessage(data.message)
+      } setLoading(false);
+      if(res.ok) {
+        setErrorMessage(null);
+        navigate('/signin');
+      }
+    } catch (error){
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className='min-h-screen flex justify-center items-center bg-gray-100 dark:bg-gray-800'>
       <div className='max-w-4xl w-full p-8 bg-white shadow-lg rounded-lg'>
@@ -33,21 +72,28 @@ export default function SignUp() {
         </div>
 
         <div className='text-center'>
-          <form className='inline-block w-full max-w-md mx-auto'>
+          <form className='inline-block w-full max-w-md mx-auto' onSubmit={handleSubmit}>
             <div className='mb-4'>
               <Label htmlFor='username' value="Your username" />
-              <TextInput type='text' placeholder='username' id='username' className='w-full' />
+              <TextInput type='text' placeholder='username' id='username' className='w-full' onChange={handleChange}/>
             </div>
             <div className='mb-4'>
               <Label htmlFor='email' value="Email" />
-              <TextInput type='email' placeholder='name@company.com' id='email' className='w-full' />
+              <TextInput type='email' placeholder='name@company.com' id='email' className='w-full' onChange={handleChange}/>
             </div>
             <div className='mb-4'>
               <Label htmlFor='password' value="Password" />
-              <TextInput type='password' placeholder='********' id='password' className='w-full' />
+              <TextInput type='password' placeholder='********' id='password' className='w-full' onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToPink' className='w-full mt-4' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' className='w-full mt-4' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                  <Spinner size='sm' />
+                    <span className='p-3'> Loading... </span>
+                  </>
+                  ) : (  'Sign Up' )
+              }
             </Button>
           </form>
           <div className='mt-4'>
@@ -55,6 +101,11 @@ export default function SignUp() {
               <Link to='/signin' className='font-bold text-purple-600 dark:text-purple-400 ml-1'> Sign In</Link>
             </span>
           </div>
+          { errorMessage && ( 
+            <Alert className='mt-4' color='failure'>
+              {errorMessage}
+              </Alert>
+          )}
         </div>
       </div>
     </div>
