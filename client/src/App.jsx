80a6bcebe0d2c9de32d51fe7dc99e05be464f243
rsv13 <rsv130393@gmail.com>
2024-08-02 14:49:19 +0100

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import ContactUs from './pages/ContactUs'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Publication from './pages/Publication'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Survey from './pages/Survey'
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>
-      app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/publication" element={<Publication />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contactus" element={<ContactUs />} />
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
