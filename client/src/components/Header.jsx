9226f57f920253d07a887bb476535f6bd70de233
rsv13 <rsv130393@gmail.com>
2024-08-03 17:20:53 +0100

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,59 +1,75 @@
-import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
-import React from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
-import { Link, useLocation } from 'react-router-dom'
-
+import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector(state => state.user);
 
-  const path = useLocation().pathname
+  console.log('Current User:',  currentUser);
 
   return (
-      <Navbar className='border-b-2 self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'> 
-         <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
-              <span className='px-2 py-1 bg-red-700 rounded-lg text-white' style={{ backgroundColor: 'rgba(202, 5, 57, 255)' }}>USW</span> Survey
-            </Link>
-        <form>
-          <TextInput 
-            type='text'
-            placeholder='Search...'
-            rightIcon={AiOutlineSearch} 
-            className='hidden lg:inline' 
-          />
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-          <AiOutlineSearch />
-        </Button>
-        <div className='flex gap-2 md:order-2'>
+    <Navbar className='border-b-2 self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'> 
+      <Link to='/'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>SWSWBS</span> Survey
+      </Link>
+      <form>
+        <TextInput 
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch} 
+          className='hidden lg:inline' 
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-          </Link>
-       </div>
-       <NavbarToggle />
-          <Navbar.Collapse>
-            <Navbar.Link active={path === '/'} as={'div'}>
-              <Link to='/'>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/survey'} as={'div'}>
-              <Link to='/survey'>Survey</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/about'} as={'div'}>
-              <Link to='/about'>About Us</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/projects'} as={'div'}>
-              <Link to='/projects'> Projects</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/contactus'} as={'div'}>
-              <Link to='/contactus'>Contact Us</Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
-      </Navbar>
+        { currentUser ? (
+          <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded />}>
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
 
-  )
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
+          </Link>
+        )}
+        <NavbarToggle />
+      </div>  
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/survey'} as={'div'}>
+          <Link to='/survey'>Survey</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About Us</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/contactus'} as={'div'}>
+          <Link to='/contactus'>Contact Us</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  currentState: null,
+  currentUser: null,
   error: null,
   loading: false,
 };
@@ -15,7 +15,7 @@ const userSlice = createSlice({
       state.loading = true;
     },
     signInSuccess: (state, action) => {
-      state.currentState = action.payload;
+      state.currentUser = action.payload;
       state.loading = false;
       state.error = null;
     },
