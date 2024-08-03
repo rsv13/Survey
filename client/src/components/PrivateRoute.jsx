c8615e1b5a85419db2577645eb96ba0d571d2b07
rsv13 <rsv130393@gmail.com>
2024-08-03 20:27:28 +0100

    Make the dashboard private

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,7 +4,13 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
 
+<<<<<<< HEAD
     const { currentUser } = useSelector ((state) => state.user);
     return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
 
+=======
+    const { currentUser } = useSelector(state => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to = '/sign-in' />;
+>>>>>>> 902bd37 (Make the dashboard private)
 }
