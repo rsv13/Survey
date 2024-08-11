4e08613af485bba747316f1feb1a3709b3a21a3a
rsv13 <rsv130393@gmail.com>
2024-08-11 21:31:58 +0100

    Viewing Survey of the particular user

---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,233 @@
+import { Button, Modal, Table } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
+import { FaCheck, FaTimes } from 'react-icons/fa';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default function DashUsers() {
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.user);
+  const [users, setUsers] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState('');
+  const [selectedUserSurveys, setSelectedUserSurveys] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [showSurveyModal, setShowSurveyModal] = useState(false);
+
+  const formatDate = (dateString) => {
+    const [month, day, year] = dateString.split('/');
+    return `${day.padStart(2, '0')}/${month.padStart(2, '0')}/${year}`;
+  };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/getusers`);
+        const data = await res.json();
+        if (res.ok) {
+          setUsers(data.users);
+          if (data.users.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchUsers();
+    }
+  }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = users.length;
+    try {
+      const res = await fetch(`/api/user/getusers?startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => [...prev, ...data.users]);
+        if (data.users.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowDeleteModal(false);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleUserClick = async (userId) => {
+    try {
+      const res = await fetch(`/api/survey/getSurveys?userId=${userId}`);
+      const data = await res.json();
+      if (res.ok) {
+        setSelectedUserSurveys(data.surveys);
+        setShowSurveyModal(true);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleViewClick = (survey) => {
+    navigate('/survey-details', { state: { survey } });
+  };
+
+  return (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && users.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date Created</Table.HeadCell>
+              <Table.HeadCell>User Image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
+              <Table.HeadCell>Admin</Table.HeadCell>
+              <Table.HeadCell>View Surveys</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+            </Table.Head>
+            {users.map((user) => (
+              <Table.Body className='divide-y' key={user._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  <Table.Cell>
+                    {formatDate(new Date(user.createdAt).toLocaleDateString())}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <img
+                      src={user.profilePicture}
+                      alt={user.username}
+                      className='w-10 h-10 object-cover bg-gray-500 rounded-full'
+                    />
+                  </Table.Cell>
+                  <Table.Cell>{user.username}</Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>
+                    {user.isAdmin ? (
+                      <FaCheck className='text-green-500' />
+                    ) : (
+                      <FaTimes className='text-red-500' />
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Button onClick={() => handleUserClick(user._id)}>
+                      View Surveys
+                    </Button>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowDeleteModal(true);
+                        setUserIdToDelete(user._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
+        </>
+      ) : (
+        <p>You have no users yet!</p>
+      )}
+
+      <Modal
+        show={showDeleteModal}
+        onClose={() => setShowDeleteModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete this user?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowDeleteModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        show={showSurveyModal}
+        onClose={() => setShowSurveyModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Surveys taken by {selectedUser?.username}
+            </h3>
+            {selectedUserSurveys.length > 0 ? (
+              <Table hoverable className='shadow-md'>
+                <Table.Head>
+                  <Table.HeadCell>Survey Date</Table.HeadCell>
+                  <Table.HeadCell>Title</Table.HeadCell>
+                  <Table.HeadCell>Details</Table.HeadCell>
+                </Table.Head>
+                {selectedUserSurveys.map((survey) => (
+                  <Table.Body className='divide-y' key={survey._id}>
+                    <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                      <Table.Cell>
+                        {formatDate(new Date(survey.updatedAt).toLocaleDateString())}
+                      </Table.Cell>
+                      <Table.Cell>{survey.title}</Table.Cell>
+                      <Table.Cell>
+                        <Button onClick={() => handleViewClick(survey)}>
+                          View Details
+                        </Button>
+                      </Table.Cell>
+                    </Table.Row>
+                  </Table.Body>
+                ))}
+              </Table>
+            ) : (
+              <p>This user has not taken any surveys yet.</p>
+            )}
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
