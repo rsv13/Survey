874b9cd1b8fc499aa7733809fd3d40f3443f7b57
rSv <28686684+rsv13@users.noreply.github.com.>
2024-08-20 05:16:12 +0100

    Displaying labels on the sidebar according to the user role.

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,52 +1,51 @@
+// components/DashSidebar.jsx
+
 import { Sidebar } from 'flowbite-react';
 import React, { useEffect, useState } from 'react';
 import { FcSurvey } from "react-icons/fc";
 import { HiChartPie, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { RiShutDownLine } from "react-icons/ri";
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function DashSidebar() {
-
   const { currentUser } = useSelector((state) => state.user);
-
-
   const location = useLocation();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [tab, setTab] = useState('');
-  
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab')
+    const tabFromUrl = urlParams.get('tab');
     if (tabFromUrl) {
-      setTab(tabFromUrl)
-    } 
-},  [location.search])
-
-const handleSignout = async () => {
-
-  try {
-    const res = await fetch('api/user/signout', {
-      method: 'POST',
-    });
-    const data = await res.json()
-    if (!res.ok) {
-      console.log(data.message);
-    } else {
-      dispatch(signoutSuccess());
-      navigate('/signin');
+      setTab(tabFromUrl);
     }
-  } catch (error) {
-    console.log(error.message)
-  }
-}
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+        navigate('/signin');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-                <Sidebar.ItemGroup>
-                {currentUser && currentUser.isAdmin && (
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          {currentUser.role === 'Admin' && (
             <Link to='/dashboard?tab=dash'>
               <Sidebar.Item
                 active={tab === 'dash' || !tab}
@@ -57,36 +56,43 @@ const handleSignout = async () => {
               </Sidebar.Item>
             </Link>
           )}
-                <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'
-                  >
-                    Profile
-                    </Sidebar.Item>
-                </Link>
-                <Link to='/dashboard?tab=surveys'>
-                  <Sidebar.Item active={tab === 'surveys'} icon={FcSurvey} labelColor='dark' as='div'
-                  >
-                    Surveys
-                  </Sidebar.Item>
-                </Link>
-                {currentUser.isAdmin && (
-            <>
-              <Link to='/dashboard?tab=users'>
-                <Sidebar.Item
-                  active={tab === 'users'}
-                  icon={HiOutlineUserGroup}
-                  as='div'
-                >
-                  Users
-                </Sidebar.Item>
-              </Link>
-            </>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.role === 'Admin' ? 'Admin' : currentUser.role === 'Group Admin' ? 'Group Admin' : 'User'}
+              labelColor='dark'
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Link to='/dashboard?tab=surveys'>
+            <Sidebar.Item
+              active={tab === 'surveys'}
+              icon={FcSurvey}
+              labelColor='dark'
+              as='div'
+            >
+              Surveys
+            </Sidebar.Item>
+          </Link>
+          {currentUser.role === 'Admin' && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item
+                active={tab === 'users'}
+                icon={HiOutlineUserGroup}
+                as='div'
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
           )}
-                    <Sidebar.Item icon={RiShutDownLine } className='cursor-pointer' onClick={handleSignout}>
-                        Sign Out
-                    </Sidebar.Item>      
-                </Sidebar.ItemGroup>
-        </Sidebar.Items>   
+          <Sidebar.Item icon={RiShutDownLine} className='cursor-pointer' onClick={handleSignout}>
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
