ba2665162404b3556f10ea035366dbf976fc8ee5
rSv <28686684+rsv13@users.noreply.github.com.>
2024-08-21 18:07:39 +0100

    fixed the result import in the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import GroupDetails from './pages/GroupDetails';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Publication from './pages/Publication';
+import Results from './pages/Results';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Survey from './pages/Survey';
