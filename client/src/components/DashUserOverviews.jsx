ae2566d772fa5b39974a64dd499c773de299e2f4
rSv <28686684+rsv13@users.noreply.github.com.>
2024-08-30 17:51:31 +0100

    displaying user roles correctly in the admin panel

--- client/src/components/DashUserOverviews.jsx
+++ client/src/components/DashUserOverviews.jsx
@@ -177,7 +177,7 @@ export default function DashUserOverview() {
                       <Table.Cell>{new Date(user.createdAt).toLocaleDateString()}</Table.Cell>
                       <Table.Cell>{user.surveyUsername || 'N/A'}</Table.Cell>
                       <Table.Cell>{user.email || 'N/A'}</Table.Cell>
-                      <Table.Cell>{user.user?.role || 'N/A'} </Table.Cell>
+                      <Table.Cell>{user.role || 'N/A'} </Table.Cell>
                       <Table.Cell>{user.groupId?.name || 'No Group'}</Table.Cell>
                       <Table.Cell>{user.groupId?.description || 'No Description'}</Table.Cell>
                       <Table.Cell>
