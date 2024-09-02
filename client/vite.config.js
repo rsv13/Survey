bd803ece676af821e3f5945710ac5c40b4d46b0b
rSv <28686684+rsv13@users.noreply.github.com>
2024-09-02 23:35:56 +0100

    Added URL for the hosting website

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: process.env.PORT || 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.API_URL || "http://localhost:3000",
         secure: false,
       },
     },
