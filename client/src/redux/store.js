d300230f51b6c797c40c5b8186ffa72b9c7e1df9
rsv13 <rsv130393@gmail.com>
2025-07-28 07:30:10 +0100

    refactor: clean up commented-out code and ensure session storage is used

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,37 @@
+// import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// import { persistReducer, persistStore } from "redux-persist";
+
+// import storageSession from "redux-persist/lib/storage/session"; // Change to sessionStorage
+
+// import themeReducer from "./theme/themeSlice";
+// import userReducer from "./user/userSlice";
+
+// const rootReducer = combineReducers({
+//   user: userReducer,
+//   theme: themeReducer,
+// });
+
+// const persistConfig = {
+//   key: "root",
+//   storage: storageSession, // Use sessionStorage instead of localStorage
+//   version: 1,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({ serializableCheck: false }),
+// });
+
+// export const persistor = persistStore(store);
+
+
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
-<<<<<<< HEAD
-import storageSession from "redux-persist/lib/storage/session"; // Change to sessionStorage
-=======
 import storageSession from "redux-persist/lib/storage/session";
->>>>>>> 9ff8d38 (changed the storage to Session storage)
+
 import themeReducer from "./theme/themeSlice";
 import userReducer from "./user/userSlice";
 
@@ -15,11 +42,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: "root",
-<<<<<<< HEAD
-  storage: storageSession, // Use sessionStorage instead of localStorage
-=======
   storage: storageSession,
->>>>>>> 9ff8d38 (changed the storage to Session storage)
   version: 1,
 };
 
